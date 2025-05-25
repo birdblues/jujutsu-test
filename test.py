4749d9eddb7697e6c859a93715a12ea97170c56c
dhseo <birdblues@mac.com>
2025-05-25 20:28:31 +0900

    몇까지 문서 추가

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+
+print("This script will remove all files in the current directory that are not Python files.")
+print("Please ensure you are in the correct directory before proceeding.")
+confirm = input("Do you want to proceed? (yes/no): ").strip().lower()
+print("test")
